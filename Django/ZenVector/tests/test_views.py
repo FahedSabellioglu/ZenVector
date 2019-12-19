afd066b5ea52b3953f5558f4af38fff490d43c44
FahedSabellioglu <fahedshaabani@std.sehir.edu.tr>
2019-12-19 03:35:44 +0300

    projects unit tests

--- Django/ZenVector/tests/test_views.py
+++ Django/ZenVector/tests/test_views.py
@@ -13,7 +13,6 @@ class TestViews(unittest.TestCase):
 
     def test_delete_account(self):
         client = Client()
-
         response_notloggedin = client.post("/PutTogether/DeleteAccount/",{"password":"test"})
         self.assertEqual(response_notloggedin.status_code,302)
 
@@ -27,6 +26,19 @@ class TestViews(unittest.TestCase):
         client = Client()
         response = client.post("/PutTogether/Login/",{"mail":"test@gmail.com",'password':"test"})
         self.assertEqual(response.status_code,403)
+
+    def test_project_create(self):
+        client = Client()
+        urs = Users.objects.create_user(username='test',email='test@gmail.com',password='test')
+        client.login(email='test@gmail.com',password='test')
+        response = client.post('/PutTogether/Projects/CreateProject/',{"p_name":"TEST project",'members':['none']})
+        self.assertEqual(response.status_code,200)
+        urs.delete()
+
+
+
+
+
 
 
 
